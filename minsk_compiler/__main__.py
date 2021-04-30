1d14ca519298c602996b3d82e2d26ff35cbaab23
onContentStop <mariorules24@gmail.com>
2021-04-30 17:25:21 -0500

    update pretty printing

--- minsk_compiler/__main__.py
+++ minsk_compiler/__main__.py
@@ -3,16 +3,28 @@ from minsk_compiler.syntax_node import SyntaxNode
 from minsk_compiler.parser import Parser
 
 
-def pretty_print(node: SyntaxNode, indent: str = ""):
+def pretty_print(node: SyntaxNode, indent: str = "", is_last: bool = True):
     print(indent, end="")
+    if is_last:
+        print("\\..", end="")
+    else:
+        print("+..", end="")
     print(node.kind(), end="")
     if isinstance(node, SyntaxToken) and node.value is not None:
         print(f" {node.value}", end="")
 
     print()
-    indent += "    "
+    if is_last:
+        indent += "   "
+    else:
+        indent += "|  "
+
+    try:
+        last = node.children()[-1]
+    except IndexError:
+        return
     for child in node.children():
-        pretty_print(child, indent)
+        pretty_print(child, indent, child == last)
 
 
 while True:
