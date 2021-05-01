932040d0f64f2ded329b59c6bb3960b589ffbdda
onContentStop <mariorules24@gmail.com>
2021-04-30 20:48:05 -0500

    Move precedence definitions to syntax_facts.py

--- minsk_compiler/code_analysis/parser.py
+++ minsk_compiler/code_analysis/parser.py
@@ -5,6 +5,7 @@ from minsk_compiler.code_analysis.syntax.binary_expression_syntax import BinaryE
 from minsk_compiler.code_analysis.syntax.expression_syntax import ExpressionSyntax
 from minsk_compiler.code_analysis.syntax.literal_expression_syntax import LiteralExpressionSyntax
 from minsk_compiler.code_analysis.syntax.parenthesized_expression_syntax import ParenthesizedExpressionSyntax
+from minsk_compiler.code_analysis.syntax.syntax_facts import get_binary_operator_precedence
 from minsk_compiler.code_analysis.syntax.syntax_kind import SyntaxKind
 from minsk_compiler.code_analysis.syntax.syntax_token import SyntaxToken
 
@@ -60,7 +61,7 @@ class Parser:
     def _parse_expression(self, parent_precedence: int = 0) -> ExpressionSyntax:
         left = self._parse_primary_expression()
         while True:
-            precedence = Parser.get_binary_operator_precedence(self._current().kind())
+            precedence = get_binary_operator_precedence(self._current().kind())
             if precedence == 0 or precedence <= parent_precedence:
                 break
 
@@ -69,15 +70,6 @@ class Parser:
             left = BinaryExpressionSyntax(left, operator_token, right)
 
         return left
-
-    @staticmethod
-    def get_binary_operator_precedence(kind: SyntaxKind) -> int:
-        if kind in (SyntaxKind.STAR_TOKEN, SyntaxKind.SLASH_TOKEN):
-            return 2
-        elif kind in (SyntaxKind.PLUS_TOKEN, SyntaxKind.MINUS_TOKEN):
-            return 1
-        else:
-            return 0
 
     def _parse_primary_expression(self) -> ExpressionSyntax:
         if self._current().kind() == SyntaxKind.OPEN_PARENTHESIS_TOKEN:
---
+++ minsk_compiler/code_analysis/syntax/syntax_facts.py
@@ -0,0 +1,10 @@
+from minsk_compiler.code_analysis.syntax.syntax_kind import SyntaxKind
+
+
+def get_binary_operator_precedence(kind: SyntaxKind) -> int:
+    if kind in (SyntaxKind.STAR_TOKEN, SyntaxKind.SLASH_TOKEN):
+        return 2
+    elif kind in (SyntaxKind.PLUS_TOKEN, SyntaxKind.MINUS_TOKEN):
+        return 1
+    else:
+        return 0
