74eeb1b3f4cbabd47f528855d38635625b4cd20d
onContentStop <mariorules24@gmail.com>
2021-05-01 13:06:29 -0500

    Renaming

--- minsk_compiler/code_analysis/binding/binder.py
+++ minsk_compiler/code_analysis/binding/binder.py
@@ -1,13 +1,11 @@
-from typing import cast, List, Optional
+from typing import cast, List
 
 from minsk_compiler.code_analysis.binding.bound_binary_expression import BoundBinaryExpression
 from minsk_compiler.code_analysis.binding.bound_binary_operator import BoundBinaryOperator
-from minsk_compiler.code_analysis.binding.bound_binary_operator_kind import BoundBinaryOperatorKind
 from minsk_compiler.code_analysis.binding.bound_expression import BoundExpression
 from minsk_compiler.code_analysis.binding.bound_literal_expression import BoundLiteralExpression
 from minsk_compiler.code_analysis.binding.bound_unary_expression import BoundUnaryExpression
 from minsk_compiler.code_analysis.binding.bound_unary_operator import BoundUnaryOperator
-from minsk_compiler.code_analysis.binding.bound_unary_operator_kind import BoundUnaryOperatorKind
 from minsk_compiler.code_analysis.syntax.binary_expression_syntax import BinaryExpressionSyntax
 from minsk_compiler.code_analysis.syntax.expression_syntax import ExpressionSyntax
 from minsk_compiler.code_analysis.syntax.literal_expression_syntax import LiteralExpressionSyntax
@@ -42,24 +40,24 @@ class Binder:
 
     def _bind_unary_expression(self, syntax: UnaryExpressionSyntax) -> BoundExpression:
         bound_operand = self.bind_expression(syntax.operand)
-        bound_operator_kind = BoundUnaryOperator.bind(syntax.operator_token.kind(), bound_operand.type())
-        if bound_operator_kind is None:
+        bound_operator = BoundUnaryOperator.bind(syntax.operator_token.kind(), bound_operand.type())
+        if bound_operator is None:
             self.diagnostics.append(
                 f"Unary operator '{syntax.operator_token.text}' is not defined for type {bound_operand.type()}")
             return bound_operand
-        return BoundUnaryExpression(bound_operator_kind, bound_operand)
+        return BoundUnaryExpression(bound_operator, bound_operand)
 
     def _bind_binary_expression(self, syntax: BinaryExpressionSyntax) -> BoundExpression:
         bound_left = self.bind_expression(syntax.left)
         bound_right = self.bind_expression(syntax.right)
-        bound_operator_kind = BoundBinaryOperator.bind(syntax.operator_token.kind(), bound_left.type(),
-                                                       bound_right.type())
-        if bound_operator_kind is None:
+        bound_operator = BoundBinaryOperator.bind(syntax.operator_token.kind(), bound_left.type(),
+                                                  bound_right.type())
+        if bound_operator is None:
             self.diagnostics.append(
                 f"Binary operator '{syntax.operator_token.text}' is not defined for types {bound_left.type()} "
                 f"and {bound_right.type()}")
             return bound_left
-        return BoundBinaryExpression(bound_left, bound_operator_kind, bound_right)
+        return BoundBinaryExpression(bound_left, bound_operator, bound_right)
 
     def _bind_parenthesized_expression(self, syntax: ParenthesizedExpressionSyntax) -> BoundExpression:
         return self.bind_expression(syntax.expression)
