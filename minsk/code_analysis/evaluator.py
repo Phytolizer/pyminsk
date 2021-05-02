392005077c38e9987a907723d01d4c72688e4e94
onContentStop <mariorules24@gmail.com>
2021-05-02 09:37:14 -0500

    Refactor evaluator.py

--- minsk/code_analysis/evaluator.py
+++ minsk/code_analysis/evaluator.py
@@ -26,51 +26,64 @@ class Evaluator:
     def _evaluate_expression(self, root: BoundExpression) -> Any:
         if root.kind() == BoundNodeKind.LITERAL_EXPRESSION:
             root = cast(BoundLiteralExpression, root)
-            return root.value
+            return self._evaluate_literal_expression(root)
         elif root.kind() == BoundNodeKind.VARIABLE_EXPRESSION:
             root = cast(BoundVariableExpression, root)
-            return self._variables[root.variable]
+            return self._evaluate_variable_expression(root)
         elif root.kind() == BoundNodeKind.ASSIGNMENT_EXPRESSION:
             root = cast(BoundAssignmentExpression, root)
-            value = self._evaluate_expression(root.expression)
-            self._variables[root.variable] = value
-            return value
+            return self._evaluate_assignment_expression(root)
         elif root.kind() == BoundNodeKind.UNARY_EXPRESSION:
             root = cast(BoundUnaryExpression, root)
-            operand = self._evaluate_expression(root.operand)
-
-            if root.operator.kind == BoundUnaryOperatorKind.IDENTITY:
-                return operand
-            elif root.operator.kind == BoundUnaryOperatorKind.NEGATION:
-                return -operand
-            elif root.operator.kind == BoundUnaryOperatorKind.LOGICAL_NEGATION:
-                return not operand
-            else:
-                raise Exception(f"unexpected unary operator {root.operator.kind}")
-
+            return self._evaluate_unary_expression(root)
         elif root.kind() == BoundNodeKind.BINARY_EXPRESSION:
             root = cast(BoundBinaryExpression, root)
-            left = self._evaluate_expression(root.left)
-            right = self._evaluate_expression(root.right)
-
-            if root.operator.kind == BoundBinaryOperatorKind.ADDITION:
-                return left + right
-            elif root.operator.kind == BoundBinaryOperatorKind.SUBTRACTION:
-                return left - right
-            elif root.operator.kind == BoundBinaryOperatorKind.MULTIPLICATION:
-                return left * right
-            elif root.operator.kind == BoundBinaryOperatorKind.DIVISION:
-                return left // right
-            elif root.operator.kind == BoundBinaryOperatorKind.LOGICAL_AND:
-                return left and right
-            elif root.operator.kind == BoundBinaryOperatorKind.LOGICAL_OR:
-                return left or right
-            elif root.operator.kind == BoundBinaryOperatorKind.EQUALITY:
-                return left == right
-            elif root.operator.kind == BoundBinaryOperatorKind.INEQUALITY:
-                return left != right
-            else:
-                raise Exception(f"unexpected binary operator {root.operator}")
-
+            return self._evaluate_binary_expression(root)
         else:
             raise Exception(f"unexpected node {root.kind()}")
+
+    def _evaluate_literal_expression(self, root: BoundLiteralExpression):
+        return root.value
+
+    def _evaluate_variable_expression(self, root: BoundVariableExpression):
+        return self._variables[root.variable]
+
+    def _evaluate_assignment_expression(self, root: BoundAssignmentExpression):
+        value = self._evaluate_expression(root.expression)
+        self._variables[root.variable] = value
+        return value
+
+    def _evaluate_unary_expression(self, root: BoundUnaryExpression):
+        operand = self._evaluate_expression(root.operand)
+
+        if root.operator.kind == BoundUnaryOperatorKind.IDENTITY:
+            return operand
+        elif root.operator.kind == BoundUnaryOperatorKind.NEGATION:
+            return -operand
+        elif root.operator.kind == BoundUnaryOperatorKind.LOGICAL_NEGATION:
+            return not operand
+        else:
+            raise Exception(f"unexpected unary operator {root.operator.kind}")
+
+    def _evaluate_binary_expression(self, root: BoundBinaryExpression):
+        left = self._evaluate_expression(root.left)
+        right = self._evaluate_expression(root.right)
+
+        if root.operator.kind == BoundBinaryOperatorKind.ADDITION:
+            return left + right
+        elif root.operator.kind == BoundBinaryOperatorKind.SUBTRACTION:
+            return left - right
+        elif root.operator.kind == BoundBinaryOperatorKind.MULTIPLICATION:
+            return left * right
+        elif root.operator.kind == BoundBinaryOperatorKind.DIVISION:
+            return left // right
+        elif root.operator.kind == BoundBinaryOperatorKind.LOGICAL_AND:
+            return left and right
+        elif root.operator.kind == BoundBinaryOperatorKind.LOGICAL_OR:
+            return left or right
+        elif root.operator.kind == BoundBinaryOperatorKind.EQUALITY:
+            return left == right
+        elif root.operator.kind == BoundBinaryOperatorKind.INEQUALITY:
+            return left != right
+        else:
+            raise Exception(f"unexpected binary operator {root.operator}")
